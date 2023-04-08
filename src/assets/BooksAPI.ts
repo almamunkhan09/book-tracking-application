c432031db9e3cf67256d6c352207a0b04a168f05
Al Mamun Khan <almamunkhan09@gmail.com>
2023-04-08 04:27:18 +0200

    Start typedoc in the codebase

--- src/assets/BooksAPI.ts
+++ src/assets/BooksAPI.ts
@@ -9,15 +9,34 @@ const headers = {
   Authorization: token,
 };
 
+/**
+ * Api the all the information of a book .
+ * @param bookId the unique id of the book
+ * @returns array all the information of the book
+ */
+
 export const get = (bookId: string) =>
   fetch(`${api}/books/${bookId}`, { headers })
     .then((res) => res.json())
     .then((data) => data.book);
 
+/**
+ * Api call to get all the books .
+ *
+ * @returns array of all the books.
+ */
+
 export const getAll = () =>
   fetch(`${api}/books`, { headers })
     .then((res) => res.json())
     .then((data) => data.books);
+
+/**
+ * Api call to update the shelf property of a book .
+ * @param book the bject of the book
+ * @param shelf the shelf where the book need to be moved
+ * @returns void
+ */
 
 export const update = (book: any, shelf: string) =>
   fetch(`${api}/books/${book.id}`, {
@@ -28,6 +47,13 @@ export const update = (book: any, shelf: string) =>
     },
     body: JSON.stringify({ shelf }),
   }).then((res) => res.json());
+
+/**
+ * Api call to update the shelf property of a book .
+ * @param query the search parameter
+ * @param maxResults set te number of output
+ * @returns array of books as per input search parameters
+ */
 
 export const search = (query: string, maxResults: any) =>
   fetch(`${api}/search`, {
--- src/assets/functions.ts
+++ src/assets/functions.ts
@@ -1,16 +1,44 @@
-import { Book } from './types';
+import { Book } from './types'; // Type declaration of book
+
+/**
+ * Find all the books which are in currently reading shelf.
+ *
+ * @param array all the books coming from the api calling.
+ * @returns array of books which are in currently reading shelf.
+ */
 
 export const findCurrentlyReading = (array: Book[]): Book[] => {
   return array.filter((item) => item.shelf === 'currentlyReading');
 };
 
+/**
+ * Find all the books which are in want to read shelf.
+ *
+ * @param array all the books coming from the api calling.
+ * @returns array of books which are in want to read shelf.
+ */
+
 export const findWantToRead = (array: Book[]): Book[] => {
   return array.filter((item) => item.shelf === 'wantToRead');
 };
 
+/**
+ * Find all the books which are in read shelf.
+ *
+ * @param array all the books coming from the api calling.
+ * @returns array of books which are in read shelf.
+ */
+
 export const findRead = (array: Book[]): Book[] => {
   return array.filter((item) => item.shelf === 'read');
 };
+
+/**
+ * Find all the books which are in none .
+ *
+ * @param array all the books coming from the api calling.
+ * @returns array of books which are in none.
+ */
 
 export const findNone = (array: Book[]): Book[] => {
   return array.filter((item) => item.shelf === 'none');
